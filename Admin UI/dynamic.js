cc084c81c884383158e05bd2816088202593fee5
kaarthikeyan <kaarthikeyan.b@gradious.com>
2026-05-04 12:38:28 +0530

    dynamic conflict removed

--- Admin UI/dynamic.js
+++ Admin UI/dynamic.js
@@ -4273,16 +4273,8 @@ document.addEventListener("DOMContentLoaded", async function () {
     candidatesData = await getDataFromDb(constituencyId, constState);
     console.log("candidatesData:", candidatesData);
 
-<<<<<<< Updated upstream
     const candidateDetails = candidatesData.filter((candidate) =>
       parties.includes(candidate.party.trim())
-=======
-    const candidateDetails = candidatesData.filter((candidate) =>{
-      console.log("cand:",cand);
-      
-      parties.includes(candidate.party)
-    }
->>>>>>> Stashed changes
     );
 
 
